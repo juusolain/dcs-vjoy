69df0a62b98f7b74197f1b466f822034bec724bc
jusola <juusolain@gmail.com>
2019-04-19 19:47:39 +0300

    Initial

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 ws = new WebSocket('ws://localhost:40000');
+var plaunchpad = require('phi-launchpad');
+var nodeMIDI = require('node_midi_interface.js')
+
+var midiDev = new dummyMIDI.midi():
+
+var lpadIn = new plaunchpad.input();
+lpadIn.init(midiDev);
+
+var lpadOut = new plaunchpad.output();
+lpadOut.init(midiDev);
 
 ws.onopen = function() {
   console.log('WebSocket connection opened');
@@ -10,13 +20,17 @@ ws.onclose = function() {
 
 
 function pressButton(lpID){
-
+  let buttonID = lpIDtoNum(lpID);
+  ws.send("set output "+buttonID+" 1.0");
 }
 
 function releaseButton(lpID){
-
+  let buttonID = lpIDtoNum(lpID);
+  ws.send("set output "+buttonID+" 0.0");
 }
 
 function lpIDtoNum(lpID){
-  
+  if(lpID < 8){
+    return lpID;
+  }
 }
