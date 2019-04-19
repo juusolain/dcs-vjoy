5a3367a3fb6440bb90a7028308fd79390910cab6
jusola <juusolain@gmail.com>
2019-04-19 19:41:18 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,22 @@
+ws = new WebSocket('ws://localhost:40000');
+
+ws.onopen = function() {
+  console.log('WebSocket connection opened');
+}
+
+ws.onclose = function() {
+  console.log('WebSocket connection closed')
+}
+
+
+function pressButton(lpID){
+
+}
+
+function releaseButton(lpID){
+
+}
+
+function lpIDtoNum(lpID){
+  
+}
