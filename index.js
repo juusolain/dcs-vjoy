403cd79f0fb3bf2e21934852629e25b945467df7
jusola <juusolain@gmail.com>
2019-04-20 18:46:33 +0300

    Add some files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const WebSocket = require("isomorphic-ws")
+
 ws = new WebSocket('ws://localhost:40000');
 var plaunchpad = require('phi-launchpad');
-var nodeMIDI = require('node_midi_interface.js')
+var nodeMIDI = require('./node_midi_interface.js')
 
-var midiDev = new dummyMIDI.midi():
+var midiDev = new nodeMIDI.midi();
 
 var lpadIn = new plaunchpad.input();
 lpadIn.init(midiDev);
@@ -19,18 +21,22 @@ ws.onclose = function() {
 }
 
 
-function pressButton(lpID){
-  let buttonID = lpIDtoNum(lpID);
+function pressButton(buttonID){
   ws.send("set output "+buttonID+" 1.0");
 }
 
-function releaseButton(lpID){
-  let buttonID = lpIDtoNum(lpID);
+function releaseButton(buttonID){
   ws.send("set output "+buttonID+" 0.0");
 }
 
-function lpIDtoNum(lpID){
-  if(lpID < 8){
-    return lpID;
-  }
-}
+lpadIn.on('press', (row, col)=>{
+  let id = col + row*8 + 1;
+  console.log(id);
+  pressButton(id);
+})
+
+lpadIn.on('release', (row, col)=>{
+  let id = col + row*8 + 1;
+  console.log(id);
+  releaseButton(id);
+})
