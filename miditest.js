b7680f12338463c4fb7e986395693595fbf9e5cc
jusola <juulain@gmail.com>
2019-10-26 17:29:24 +0300

    Add stuff

--- miditest.js
+++ miditest.js
@@ -17,3 +17,24 @@ console.log(input.getPortCount());
 
 // Get the name of a specified output port.
 console.log(input.getPortName(0));
+
+// Configure a callback.
+input.on('message', (deltaTime, message) => {
+  // The message is an array of numbers corresponding to the MIDI bytes:
+  //   [status, data1, data2]
+  // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+  // information interpreting the messages.
+  console.log(`m: ${message} d: ${deltaTime}`);
+});
+
+// Open the first available input port.
+input.openPort(0);
+
+// Sysex, timing, and active sensing messages are ignored
+// by default. To enable these message types, pass false for
+// the appropriate type in the function below.
+// Order: (Sysex, Timing, Active Sensing)
+// For example if you want to receive only MIDI Clock beats
+// you should use
+// input.ignoreTypes(true, false, true)
+input.ignoreTypes(false, false, false);
