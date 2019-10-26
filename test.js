4b8da641053954ff6609909b8503555c6310928b
jusola <juulain@gmail.com>
2019-10-26 17:34:21 +0300

    Stuff

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const Launchpad = require( 'launchpad-mini' ),
       pad = new Launchpad();
 
-pad.connect(0,1).then( () => {     // Auto-detect Launchpad
+pad.connect().then( () => {     // Auto-detect Launchpad
     pad.reset( 2 );             // Make Launchpad glow yellow
+    console.log("Conencted");
     pad.on( 'key', k => {
         // Make button red while pressed, green after pressing
+        console.log(k);
         pad.col( k.pressed ? pad.red : pad.green, k );
     } );
 } );
